2ff81f7b179deda1e106da75b454d92f85ee599f
Maksymhp <111457593+Maksymhp@users.noreply.github.com>
2024-07-09 21:33:29 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,34 @@
+const menuBtn = document.querySelector ('.menu__btn');
+const  menu = document.querySelector ('.menu__list');
+
+
+menuBtn.addEventListener ('click' , () => {
+menu.classList.toggle('menu__list--active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const menuBurger = document.querySelector ('.menu__btn');
+// const  menu = document.querySelector ('.menu__list');
+
+// menuBurger.addEventListener ('click' , () => {
+// menu.classList.toogle('menu__list--active');
+// });
